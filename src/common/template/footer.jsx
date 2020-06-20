80b30606cc0959ae26f1c105a0b13e47e5fcf283
gbochikubo <gbochikubo@estudante.ufla.br>
2020-06-20 11:41:04 -0300

    Adicionado componentes de menu e rotas configuradas

---
+++ src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+    <footer className='main-footer'> 
+        <strong> 
+            Copyright &copy; 2020
+            <a href='https://github.com/gbochikubo' target='_blank'> Guilherme Ochikubo</a>.
+        </strong>
+    </footer>
+)
--- src/main/app.jsx
+++ src/main/app.jsx
@@ -1,7 +1,21 @@
+import '../common/template/dependencies'
 import React from 'react'
+
+import Header from '../common/template/header'
+import SideBar from '../common/template/sideBar'
+import Footer from '../common/template/footer'
+import Routes from './routes'
+// import Messages from '../common/msg/messages'
+
 
 export default props => (
     <div className='wrapper'>
-        <h1>App</h1>
+        <Header></Header>
+        <SideBar></SideBar>
+        <div className='content-wrapper'>
+            <Routes></Routes>
+
+        </div>
+        <Footer></Footer>
     </div>
 )
